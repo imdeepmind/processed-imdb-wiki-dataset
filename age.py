2ee03cdbfac96634c1fe2a58cd18753157337350
imdeepmind <infinityatme@gmail.com>
2019-03-05 22:39:56 +0530

    updated the age.py and added comments

--- age.py
+++ age.py
@@ -1,21 +1,30 @@
+# Importing dependencies
 import pandas as pd
 import numpy as np
 import cv2
 import os
-
 from sklearn.model_selection import train_test_split
 
+# Loading dataset
 meta = pd.read_csv('meta.csv')
 
+# Dropping gender column
+meta = meta.drop(['gender'], axis=1)
+
+# Filtaring dataset
 meta = meta[meta['age'] >= 0]
 meta = meta[meta['age'] <= 101]
 
+# Converting into numpy array
 meta = meta.values
 
+# Spliting dataset into training and testing set
 D_train, D_test = train_test_split(meta, test_size=0.2, random_state=42)
 
+# Image path
 paths = {'imdb':'imdb_crop', 'wiki':'wiki_crop/'}
 
+# Making the directory structure
 for i in range(102):
     output_dir_train_male = 'dataset/age/train/' + str(i)
     output_dir_train_female = 'dataset/age/train/' + str(i)
@@ -35,10 +44,11 @@ for i in range(102):
     if not os.path.exists(output_dir_test_female):
         os.makedirs(output_dir_test_female)
 
+# Finally making the training and testing set
 counter = 0
 
 for image in D_train:
-    img = cv2.imread(image[2], 1)
+    img = cv2.imread(image[1], 1)
     img = cv2.resize(img, (128,128))
     cv2.imwrite('dataset/age/train/' + str(image[0]) + '/' + str(counter) + '.jpg', img)
     print('--('+str(counter)+')Processing--')
@@ -47,7 +57,7 @@ for image in D_train:
 counter = 0
 
 for image in D_test:
-    img = cv2.imread(image[2], 1)
+    img = cv2.imread(image[1], 1)
     img = cv2.resize(img, (128,128))
     cv2.imwrite('dataset/age/test/' + str(image[0]) + str(counter) + '.jpg', img)
     print('--('+str(counter)+')Processing--')
