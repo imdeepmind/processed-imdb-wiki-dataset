e07aae5dc7978971ddaad1a03f480c73a4d133a4
imdeepmind <infinityatme@gmail.com>
2019-03-05 22:20:38 +0530

    update gender.py

--- gender.py
+++ gender.py
@@ -2,14 +2,34 @@ import pandas as pd
 import numpy as np
 import cv2
 import os
-
 from sklearn.model_selection import train_test_split
 
 meta = pd.read_csv('meta.csv')
+meta = meta.drop(['age'], axis=1)
 
-meta = meta.values
+D_train, D_test = train_test_split(meta, test_size=0.1, random_state=42)
 
-D_train, D_test = train_test_split(meta, test_size=0.2, random_state=42)
+D_train_male = D_train[D_train['gender'] == 'male']
+D_train_female = D_train[D_train['gender'] == 'female']
+
+no_male = len(D_train_male)
+no_female = len(D_train_female)
+
+if no_male > no_female:
+    extra = D_train_male[no_female:]
+    D_train_male = D_train_male[0:no_female]
+
+    D_test = pd.concat((D_test, extra))
+else:
+    extra = D_train_male[no_male:]
+    D_train_male = D_train_male[0:no_male]
+
+    D_test = pd.concat((D_test, extra))
+
+D_train = pd.concat((D_train_male, D_train_female))
+
+D_train = D_train.sample(frac=1)
+D_test = D_test.sample(frac=1)
 
 paths = {'imdb':'imdb_crop', 'wiki':'wiki_crop/'}
 
@@ -31,26 +51,24 @@ if not os.path.exists(output_dir_test_male):
 if not os.path.exists(output_dir_test_female):
     os.makedirs(output_dir_test_female)
 
-
 counter = 0
 
-for image in D_train:
-    img = cv2.imread(image[2], 1)
+for image in D_train.values:
+    img = cv2.imread(image[1], 1)
     img = cv2.resize(img, (128,128))
-    if image[1] == 'male':
+    if image[0] == 'male':
         cv2.imwrite('dataset/gender/train/male/' + str(counter) + '.jpg', img)
     else:
         cv2.imwrite('dataset/gender/train/female/' + str(counter) + '.jpg', img)
     print('--('+str(counter)+')Processing--')
     counter += 1
 
-
 counter = 0
 
-for image in D_test:
-    img = cv2.imread(paths[image[3]] + '/' + image[2], 1)
+for image in D_test.values:
+    img = cv2.imread(paths[image[3]] + '/' + image[1], 1)
     img = cv2.resize(img, (128,128))
-    if image[1] == 'male':
+    if image[0] == 'male':
         cv2.imwrite('dataset/gender/test/male/' + str(counter) + '.jpg', img)
     else:
         cv2.imwrite('dataset/gender/test/female/' + str(counter) + '.jpg', img)
